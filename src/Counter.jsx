f7ceaabcadd2cdb8d7e59f9aeb43ecbe983fc7bb
ksooooza <ksoooza@gmail.com>
2023-01-05 14:32:38 -0500

    first commit

---
+++ src/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function Counter() {
+    const [count, setCount] = useState(0)
+
+    const increment = () => setCount(count + 1)
+    const decrement = () => setCount(count - 1)
+
+    return (
+        <div>
+            <span>Current Count: ${count}</span>
+            <button onClick={increment}> + </button>
+            <button onClick={decrement}> - </button>
+        </div>
+    )
+}
+
+export default Counter
