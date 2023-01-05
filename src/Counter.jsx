372207da6c380dd798c738eb6fd7bc393ef9cba2
ksooooza <ksoooza@gmail.com>
2023-01-05 14:42:26 -0500

    Plus 10 button

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,12 +5,18 @@ function Counter() {
 
     const increment = () => setCount(count + 1)
     const decrement = () => setCount(count - 1)
+    const plusTen = () => setCount(count + 10)
+    const minusTen = () => setCount(count - 10)
 
     return (
         <div>
             <span>Current Count: ${count}</span>
+            <br />
             <button onClick={increment}> + </button>
             <button onClick={decrement}> - </button>
+            <br />
+            <button onClick={plusTen}> +10 </button>
+            <button onClick={minusTen}> -10 </button>
         </div>
     )
 }
